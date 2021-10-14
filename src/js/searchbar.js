9096f2dbe98d0a54c7b953345c9816d90fabccc1
cguibourg-58 <42938472+cguibourg-58@users.noreply.github.com>
2021-10-14 22:19:08 +0200

    add search menu

---
+++ src/js/searchbar.js
@@ -0,0 +1,9 @@
+function hideUnhide() {
+    var searchbar = document.getElementById("searchbar");
+    if(searchbar.style.display === "none") {
+        searchbar.style.display = "block";
+    }
+    else {
+        searchbar.style.display = "none";
+    }
+}
